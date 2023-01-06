e84f49bf7f099d6287af23780ce2caab2317b750
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2023-01-06 22:16:43 +0700

    test array
    test array

--- csharp-notebooks/csharp-notebooks/Program.cs
+++ csharp-notebooks/csharp-notebooks/Program.cs
@@ -6,20 +6,32 @@ using System.Threading.Tasks;
 
 namespace csharp_notebooks
 {
+    class notebooks_array
+    {
+        private int[] array_data = { 1, 2, 3, 4, 5 };
+
+        public void find_value_array()
+        {
+            int val = array_data.Where(item => item == 1).First();
+            Console.WriteLine($"1 => {val}");
+        }
+
+        public void filter_value_array()
+        {
+            int[] val = array_data.Where(item => item > 1).ToArray();
+            Console.WriteLine(string.Join(" ", val));
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            var notebooks_array_data = new notebooks_array();
+            notebooks_array_data.filter_value_array();
 
-            int[] a = { 1, 2, 3, 4, 5 };
-           
-
-            int[] b = a.Where(item => item > 2).ToArray();
-
-            Console.WriteLine(string.Join("\n", b));
 
             Console.ReadLine();
 
-        }
+        } 
     }
 }
