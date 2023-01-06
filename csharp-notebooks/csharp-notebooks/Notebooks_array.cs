d4f654fcc65bc01f4f548593ea269335c6c1dc61
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2023-01-06 23:11:45 +0700

    add class noteboks_array
    add class noteboks_array

---
+++ csharp-notebooks/csharp-notebooks/Notebooks_array.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_notebooks
+{
+    internal class Notebooks_array
+    {
+        private int[] array_data = { 1, 2, 3, 4, 5 };
+
+        public void find_value_array()
+        {
+            int val = array_data.Where(item => item == 1).First();
+            Console.WriteLine($"1 => {val}");
+        }
+
+        public void filter_value_array()
+        {
+            int[] val = array_data.Where(item => item > 1).ToArray();
+            Console.WriteLine(string.Join(" ", val));
+        }
+
+        public void check_value_in_array(int number)
+        {
+            if (array_data.Contains(number))
+            {
+                Console.WriteLine("found Data !!");
+            }
+            else
+            {
+                Console.WriteLine("Not found Data !!");
+            }
+        }
+    }
+}
--- csharp-notebooks/csharp-notebooks/Program.cs
+++ csharp-notebooks/csharp-notebooks/Program.cs
@@ -6,29 +6,12 @@ using System.Threading.Tasks;
 
 namespace csharp_notebooks
 {
-    class notebooks_array
-    {
-        private int[] array_data = { 1, 2, 3, 4, 5 };
-
-        public void find_value_array()
-        {
-            int val = array_data.Where(item => item == 1).First();
-            Console.WriteLine($"1 => {val}");
-        }
-
-        public void filter_value_array()
-        {
-            int[] val = array_data.Where(item => item > 1).ToArray();
-            Console.WriteLine(string.Join(" ", val));
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            var notebooks_array_data = new notebooks_array();
-            notebooks_array_data.filter_value_array();
-
+            var notebooks_array_data = new Notebooks_array();
+            notebooks_array_data.check_value_in_array(7);
 
             Console.ReadLine();
 
